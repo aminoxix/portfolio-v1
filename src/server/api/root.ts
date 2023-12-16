9b151be51917b0bc231cedaf4fc5384804674d8a
aminoxix <anshumaankrprasad76@gmail.com>
2023-12-17 01:52:47 +0530

    feat: testimonial schema added & removed post
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 
-import { postRouter } from "~/server/api/routers/post";
 import { clickRouter } from "~/server/api/routers/click";
 
 /**
@@ -9,7 +8,6 @@ import { clickRouter } from "~/server/api/routers/click";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   click: clickRouter,
 });
 
