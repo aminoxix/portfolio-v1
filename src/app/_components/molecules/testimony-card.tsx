104b8ac35a578435889ff674dbde53895164f242
aminos <75872316+aminoxix@users.noreply.github.com>
2024-01-15 18:28:38 +0530

    fix: update example env and styling in testimony card and layout components (#5)
    * fix: testimonial ui (#4)
    
    * feat: testimonial creator ui
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>
    
    * feat: creator page updated with integration, ps: update mutation in future
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>
    
    * sanitization: unused packages removed
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>
    
    * enhancement: position for form, timeline padding for wrap, avatar url after upload & wip: carousel
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>
    
    * feat: simple testimonial ui
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>
    
    * fix: image border radius, eager loading, loading state for upload & width of testimonial card
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>
    
    ---------
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>
    
    * fix: update example env and styling in testimony card and layout components
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>
    
    ---------
    
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>

--- src/app/_components/molecules/testimony-card.tsx
+++ src/app/_components/molecules/testimony-card.tsx
@@ -13,9 +13,9 @@ const TestimonyCard = ({ testimonial }: { testimonial: Testimonial }) => {
   const screenWidth = useScreenWidth();
 
   return (
-    <div className="flex min-h-[303px] w-full cursor-pointer flex-col justify-between gap-16 rounded-xl border-4 border-double border-secondary bg-primary bg-opacity-10 bg-clip-padding p-7 backdrop-blur-sm backdrop-filter md:h-[388px]">
+    <div className="flex min-h-[303px] w-full cursor-pointer flex-col justify-between gap-16 rounded-xl border-4 border-double border-secondary bg-primary bg-opacity-10 bg-clip-padding p-3 backdrop-blur-sm backdrop-filter md:h-[388px] md:p-7">
       <p className={`text-justify text-sm md:text-base ${obviously.className}`}>
-        {testimonial.testimony}
+        {testimonial.testimony.toLowerCase()}
       </p>
       <div className="flex w-full items-center justify-end gap-2 md:gap-4">
         <Image
@@ -28,9 +28,12 @@ const TestimonyCard = ({ testimonial }: { testimonial: Testimonial }) => {
         />
         <div className="flex flex-col">
           <Link href={testimonial.social} target="_blank">
-            <Subtitle className="text-primary" text={testimonial.name} />
+            <Subtitle
+              className="text-primary"
+              text={testimonial.name.toLowerCase()}
+            />
           </Link>
-          <Description text={testimonial.position} />
+          <Description text={testimonial.position.toLowerCase()} />
         </div>
       </div>
     </div>
