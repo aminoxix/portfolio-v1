6bbeef66479a35659c4036cc77816b6cda505e42
aminoxix <anshumaankrprasad76@gmail.com>
2025-01-20 15:56:05 +0530

    feat: joined new company on sept '24
    Signed-off-by: aminoxix <anshumaankrprasad76@gmail.com>

--- src/app/_components/molecules/company/main.tsx
+++ src/app/_components/molecules/company/main.tsx
@@ -7,7 +7,10 @@ const CompanyMain = ({ company }: { company: WorkExperience }) => {
         <h1 className="text-2xl font-semibold text-primary md:text-3xl">
           {company.role}
         </h1>
-        <p className="hidden text-white md:block">{company.tenure} months</p>
+        <p className="hidden text-white md:block">
+          {company.tenure}
+          {company.tenure !== "present" && "months"}
+        </p>
       </div>
       <ul className="flex flex-col gap-3 text-white">
         {company.responsibilities.map((responsibility) => (
